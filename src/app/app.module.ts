ec995f24c9804aa6e72d7ef84ab48ad6340a23a7
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2020-01-27 19:50:50 +0100

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {StoreModule} from "@ngrx/store";
+import {EffectsModule} from "@ngrx/effects";
+import {HttpClientModule} from "@angular/common/http";
+
+import { JwPaginationComponent } from 'jw-angular-pagination';
+import { SeriesService } from './services/series.service';
+import { SeriesEffect } from './effects/series.effect';
+import { reducers } from './reducers/index';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { CartComponent } from './components/cart/cart.component';
+import  {TruncatePipe} from "./params/truncate.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    JwPaginationComponent,
+    CartComponent,
+    TruncatePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers),
+  EffectsModule.forRoot([SeriesEffect])
   ],
-  providers: [],
+  providers: [SeriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
