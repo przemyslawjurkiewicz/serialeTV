600d3784c00de3bfcc5eb3a8e5c65410bc57bc02
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2020-01-27 19:49:44 +0100

    Add Oservable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/internal/Observable';
+import {Store} from '@ngrx/store';
+
+import {SeriesService} from './services/series.service';
+import {Series} from './models/series.model';
+import * as fromApp from './reducers/series.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'serialeTV2';
+export class AppComponent  {
+   series$: Observable<Series[]>;
+  constructor(private productService: SeriesService,
+              private store: Store<fromApp.SeriesState>
+              ) {
+  }
 }
