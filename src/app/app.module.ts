6351bd7db0b1fbadd2fe346e27f4bc639a61e529
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2020-01-28 20:23:24 +0100

    add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {StoreModule} from "@ngrx/store";
 import {EffectsModule} from "@ngrx/effects";
 import {HttpClientModule} from "@angular/common/http";
-
-import { JwPaginationComponent } from 'jw-angular-pagination';
-import { SeriesService } from './services/series.service';
-import { SeriesEffect } from './effects/series.effect';
-import { reducers } from './reducers/index';
-import { AppComponent } from './app.component';
-import { ListComponent } from './components/list/list.component';
-import { CartComponent } from './components/cart/cart.component';
-import  {TruncatePipe} from "./params/truncate.pipe";
+import {JwPaginationComponent} from 'jw-angular-pagination';
+import {SeriesService} from './services/series.service';
+import {SeriesEffect} from './effects/series.effect';
+import {reducers} from './reducers/index';
+import {AppComponent} from './app.component';
+import {ListComponent} from './components/list/list.component';
+import {CartComponent} from './components/cart/cart.component';
+import {TruncatePipe} from "./params/truncate.pipe";
+import { BarRatingModule } from "ngx-bar-rating";
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent,
-    JwPaginationComponent,
-    CartComponent,
-    TruncatePipe
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    StoreModule.forRoot(reducers),
-  EffectsModule.forRoot([SeriesEffect])
-  ],
-  providers: [SeriesService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListComponent,
+        JwPaginationComponent,
+        CartComponent,
+        TruncatePipe,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([SeriesEffect]),
+        BarRatingModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [SeriesService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
