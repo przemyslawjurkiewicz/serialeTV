6f83820d8462f4ebd967e251a427c75e80e1e2bb
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2020-01-29 19:14:57 +0100

    Add search to effects

--- src/app/effects/series.effect.ts
+++ src/app/effects/series.effect.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/internal/Observable';
 import {catchError, map, switchMap, withLatestFrom} from 'rxjs/operators';
-import {Action, Store, select} from '@ngrx/store';
+import {Action, select, Store} from '@ngrx/store';
 import {Actions, Effect, ofType} from '@ngrx/effects';
 import * as fromSeries from '../reducers/index';
 import {SeriesService} from '../services/series.service';
@@ -11,13 +11,6 @@ import {empty} from 'rxjs/internal/observable/empty';
 
 @Injectable()
 export class SeriesEffect {
-    constructor(
-        private SeriesService: SeriesService,
-        private action$: Actions,
-        private store: Store<any>
-    ) {
-    }
-
     @Effect()
     loadSeries$: Observable<Action> = this.action$.pipe(
         ofType(seriesAction.SeriesActionTypes.Load),
@@ -26,7 +19,6 @@ export class SeriesEffect {
             if (loaded) {
                 return empty();
             }
-            console.log('LOADING DATA', loaded);
             return this.SeriesService.getAllSeries().pipe(
                 map(series => {
                     return new seriesAction.LoadSuccess(series);
@@ -35,6 +27,25 @@ export class SeriesEffect {
             );
         })
     );
+    @Effect()
+    search$: Observable<Action> = this.action$.pipe(
+        ofType(seriesAction.SeriesActionTypes.Search),
+        switchMap(({payload}) => {
+            return this.SeriesService.getSeriesSearch(payload).pipe(
+                map(series => {
+                    return new seriesAction.LoadSuccess(series);
+                }),
+                catchError(err => of(new seriesAction.LoadFail(err)))
+            );
+        })
+    );
+
+    constructor(
+        private SeriesService: SeriesService,
+        private action$: Actions,
+        private store: Store<any>,
+    ) {
+    }
 }
 
 
