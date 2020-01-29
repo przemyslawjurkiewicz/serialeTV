dd56e1e7ee0904cf210851d7e7a59ccdcb5913e7
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2020-01-29 19:13:24 +0100

    Add usubscribe on destroy

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl,ReactiveFormsModule } from "@angular/forms";
-import { debounceTime, distinctUntilChanged } from "rxjs/operators";
-import { Store } from '@ngrx/store';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {debounceTime, distinctUntilChanged} from "rxjs/operators";
+import {Store} from '@ngrx/store';
 import * as fromRoot from '../../reducers/series.reducer';
 import * as Actions from '../../actions/series.actions';
+import {ISubscription} from "rxjs-compat/Subscription";
+
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  searchField: FormControl;
-  searches: string='';
-  constructor(private store: Store<fromRoot.SeriesState>) { }
+    searchField: FormControl;
+    private subscription: ISubscription;
 
-  ngOnInit() {
-    this.searchField = new FormControl();
-    this.searchField.valueChanges
-        .pipe(
-            debounceTime(400),
-            distinctUntilChanged()
-        )
-        .subscribe(term => {
-          this.store.dispatch(new Actions.Search(this.searchField.value))
-        });
-    console.log(this.searches)
-     }
+    constructor(private store: Store<fromRoot.SeriesState>) {
+    }
 
+    ngOnInit() {
+        this.searchField = new FormControl();
+        this.subscription = this.searchField.valueChanges
+            .pipe(
+                debounceTime(400),
+                distinctUntilChanged()
+            )
+            .subscribe(term => {
+                (!this.searchField.value) ? this.store.dispatch(new Actions.Load()) :
+                    this.store['loaded'] = true;
+                this.store['series'] = [];
+                this.store.dispatch(new Actions.Search(this.searchField.value))
+            });
+        console.log(this.searchField.value)
+    }
 
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
+
+
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/internal/Observable';
 import {select, Store} from '@ngrx/store';
-
 import {SeriesService} from '../../services/series.service';
 import * as seriesActions from '../../actions/series.actions';
 import {Series} from '../../models/series.model';
 import * as fromApp from '../../reducers/series.reducer';
 import * as fromSeries from '../../reducers/index';
+import {ISubscription} from "rxjs-compat/Subscription";
 
 @Component({
     selector: 'app-list',
@@ -17,6 +17,7 @@ export class ListComponent implements OnInit {
     series$: Observable<Series[]>;
     pageOfItems: Array<any>;
     items: Array<any>;
+    private subscription: ISubscription;
 
     constructor(private productService: SeriesService,
                 private store: Store<fromApp.SeriesState>,
@@ -26,10 +27,15 @@ export class ListComponent implements OnInit {
     ngOnInit() {
         this.store.dispatch(new seriesActions.Load());
         this.series$ = this.store.pipe(select(fromSeries.getSeries));
-        this.series$.subscribe(data => {
+        this.subscription = this.series$.subscribe(data => {
             this.items = data;
             this.items.sort((a, b) => a.name.localeCompare(b.name));
+           // console.log(this.items);
         });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
     onChangePage(pageOfItems: Array<any>) {
